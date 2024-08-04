a83e94044b08bfff88cbe81c00e2aeae2e0c48ff
sabir222 <mrsabir4@gmail.com>
2024-08-04 18:36:28 +0100

    init commit

--- internal/server/server.go
+++ internal/server/server.go
@@ -3,10 +3,13 @@ package server
 import (
 	"github.com/Sabir222/backend-go/internal/api"
 	"net/http"
+	"path/filepath"
 )
 
 func NewServer() *http.ServeMux {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/hello", api.HelloHandler)
+	staticDir := http.Dir(filepath.Join("templates"))
+	mux.Handle("/", http.FileServer(staticDir))
 	return mux
 }
